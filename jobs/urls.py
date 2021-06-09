0c17d64759fe133a99ecd871d47e4173d06eba9b
guozhe <dylanguo916@gmail.com>
2021-06-09 20:54:35 +0800

    create front end for job details page

--- jobs/urls.py
+++ jobs/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = {
     # 职位列表
     path("joblist/", views.joblist, name="joblist"),
+    # 职位详情
+    path('job/<int:job_id>/', views.detail, name='detail'),
 }
--- jobs/views.py
+++ jobs/views.py
@@ -22,6 +22,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 import logging
 
+logger = logging.getLogger(__name__)
+
 def joblist(request):
     job_list = Job.objects.order_by('job_type')
     context =  {'job_list': job_list}
@@ -29,3 +31,12 @@ def joblist(request):
         job.city_name = Cities[job.job_city][1]
         job.type_name = JobTypes[job.job_type][1]
     return render(request, 'joblist.html', context)
+
+def detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.city_name = Cities[job.job_city][1]
+        logger.info('job retrieved from db :%s' % job_id)
+    except Job.DoesNotExist:
+        raise Http404("Job does not exist")
+    return render(request, 'job.html', {'job': job})
