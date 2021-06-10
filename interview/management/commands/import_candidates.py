bc3bac8af42ae9b81aa22e7f62d413551af0d033
guozhe <dylanguo916@gmail.com>
2021-06-10 19:15:05 +0800

    import candidate information from csv

---
+++ interview/management/commands/import_candidates.py
@@ -0,0 +1,29 @@
+import csv
+from django.core.management import BaseCommand
+from interview.models import Candidate
+
+class Command(BaseCommand):
+    help = '从一个CSV文件的内容中读取候选人列表，导入到数据库中'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--path', type=str)
+
+    def handle(self, *args, **kwargs):
+        path = kwargs['path']
+        with open(path, 'rt', encoding="gbk") as f:
+            reader = csv.reader(f, dialect='excel', delimiter=';')
+            index = 0
+            for row in reader:
+                candidate = Candidate.objects.create(
+                    username=row[0],
+                    city=row[1],
+                    phone=row[2],
+                    bachelor_school=row[3],
+                    major=row[4],
+                    degree=row[5],
+                    test_score_of_general_ability=row[6],
+                    paper_score=row[7]
+                )
+                index += 1
+                print("候选人"+str(index)+"已导入："+candidate.username)
+            print("执行成功！共导入"+str(index)+"位候选人")
